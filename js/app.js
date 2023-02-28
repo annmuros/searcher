a369a468f486014e9adee3d195ccf2573c36e7e3
annmuros <annmuros@publicisgroupe.net>
2023-02-28 16:56:42 +0100

    Genera los años del select

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 //Variables
 const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
 
+const max = new Date().getFullYear();
+const min = max -10;
+
+// console.log(max);
+// console.log(min);
 
 //eventos
 document.addEventListener('DOMContentLoaded', () =>{
-    mostrarAutos();
+    mostrarAutos();//muestra automobiles
+
+    //llena las opciones de años
+    llenarSelect();
+
 })
 
 
@@ -14,10 +24,20 @@ function mostrarAutos(){
         const {marca, modelo, year, puertas, transmision, precio, color} = auto;//Destructuring
         const autoHTML = document.createElement('p');
 
-        autoHTML.textContent= `${marca} ${modelo} - ${puertas} Puertas - Transmisión: ${transmision}`;
+        autoHTML.textContent= `${marca} ${modelo} - ${puertas} Puertas - Transmisión: ${transmision} - Color: ${color}`;
 
         //insertar en el html
         resultado.appendChild(autoHTML)
 })
 
 }
+//Genera los años del select
+function llenarSelect (){
+    for (let i = max; i>=min; i--) { //corre hacia atrás del 2023-2013
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion);// Agrega opciones de año al select
+    }
+}
+       
